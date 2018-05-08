2cfa1e8051d8619171b84315aa809f00ae8f2844
prgupta <prateek.kumar.g@gmail.com>
2018-05-08 07:57:37 +0530

    First checkin

---
+++ src/test/java/tivohackathon/tivohackathon/TivoHackathonApplicationTests.java
@@ -0,0 +1,16 @@
+package tivohackathon.tivohackathon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TivoHackathonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
