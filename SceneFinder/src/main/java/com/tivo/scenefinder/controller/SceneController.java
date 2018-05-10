2f91d61de5e1fed0bb023b7287d7600afcc58284
prgupta <prateek.kumar.g@gmail.com>
2018-05-10 12:00:57 +0530

    first API

--- SceneFinder/src/main/java/com/tivo/scenefinder/controller/SceneController.java
+++ SceneFinder/src/main/java/com/tivo/scenefinder/controller/SceneController.java
@@ -3,11 +3,11 @@ package com.tivo.scenefinder.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.JsonParseException;
+import com.tivo.scenefinder.beans.ClipSearchRequest;
 import com.tivo.scenefinder.beans.SceneMetadata;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class SceneController {
@@ -15,8 +15,8 @@ public class SceneController {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @RequestMapping("/findScene")
-    public String movieSearch(@RequestParam("movieName") String movieName, @RequestParam("category") String category){
+    @RequestMapping(value="/findScene", method = RequestMethod.POST)
+    public String movieSearch(@RequestBody ClipSearchRequest request){
 
         SceneMetadata scene = new SceneMetadata();
 
